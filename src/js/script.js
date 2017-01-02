c11d35818cb05d3a4cd74a12ab4c483e95ab0a93
Ciprian Turcu <turcuciprian1@gmail.com>
2017-01-02 11:49:17 +0200

    modified script and html for plugins

--- src/js/script.js
+++ src/js/script.js
@@ -16,10 +16,12 @@
         if (debug) {
             console.log('inside controller');
         }
-        $scope.totalUsers = '';
-        $scope.latestUser = '';
-        $scope.rootPath = '';
-        $scope.abLoginCount = '';
+        $scope.signatureOne = '';
+        $scope.scrollToTopOne = '';
+        $scope.socialIconsOne = '';
+        $scope.magnificPopupOne = '';
+        $scope.regenerateThumbnailsAdvanced = '';
+
         var abUrl = String(document.location.href);
 
         if (abUrl.indexOf('localhost') > 0) {
@@ -30,9 +32,11 @@
         }
         $scope.updatePage = function() {
 
-            $scope.totalUsers = '.....';
-            $scope.abLoginCount = '.....';
-            $scope.abSCount = '.....';
+          $scope.signatureOne = '.....';
+          $scope.scrollToTopOne = '.....';
+          $scope.socialIconsOne = '.....';
+          $scope.magnificPopupOne = '.....';
+            $scope.regenerateThumbnailsAdvanced = '.....';
             $http({
                 method: 'GET',
                 headers: {
@@ -41,16 +45,12 @@
                 url: $scope.rootPath + '/wp-json/ab/getAdminStatus',
             }).then(function successCallback(response) {
 
-                $scope.totalUsers = response.data.users.total_users;
-                $scope.abLoginCount = response.data.abLoginCount;
-                $scope.abSCount = response.data.abSCount;
-                $scope.projectInfo = response.data.projectInfo;
-                $scope.logintrackerInfo = response.data.logintrackerInfo;
-
-                console.log($scope.logintrackerInfo);
+              $scope.signatureOne = response.data.signatureOne;
+              $scope.scrollToTopOne = response.data.scrollToTopOne;
+              $scope.socialIconsOne = response.data.socialIconsOne;
+              $scope.magnificPopupOne = response.data.magnificPopupOne;
+              $scope.regenerateThumbnailsAdvanced = response.data.regenerateThumbnailsAdvanced;
                 //generate charts
-                loadChart($scope.projectInfo,'products_chart','Projects');
-                loadChart($scope.logintrackerInfo,'login_chart','Logins');
 
             }, function errorCallback(response) {
                 // called asynchronously if an error occurs
@@ -62,83 +62,5 @@
         };
         $scope.updatePage();
 
-
-
-
-        var loadChart = function(data,canvasid,title) {
-
-            window.chartColors = {
-                red: 'rgb(255, 99, 132)',
-                orange: 'rgb(255, 159, 64)',
-                yellow: 'rgb(255, 205, 86)',
-                green: 'rgb(75, 192, 192)',
-                blue: 'rgb(54, 162, 235)',
-                purple: 'rgb(153, 102, 255)',
-                grey: 'rgb(231,233,237)'
-            };
-
-            window.randomScalingFactor = function() {
-                return (Math.random() > 0.5 ? 1.0 : -1.0) * Math.round(Math.random() * 100);
-            };
-            var post_date = [];
-            var nr_of_projects = [];
-            console.log(data);
-            data.forEach(function(item) {
-              post_date.push(item.post_date);
-                nr_of_projects.push(parseInt(item.nr_of_projects));
-            });
-            console.log(post_date);
-            var maxY = getMaxOfArray(nr_of_projects);
-            console.log(nr_of_projects);
-            console.log(maxY);
-            var config = {
-                type: 'line',
-                data: {
-                    labels: post_date,
-                    datasets: [ {
-                        label: "# of "+title+" per Date registered",
-                        fill: false,
-                        backgroundColor: window.chartColors.blue,
-                        borderColor: window.chartColors.blue,
-                        data: nr_of_projects,
-                    }]
-                },
-                options: {
-                    responsive: true,
-                    title: {
-                        display: true,
-                        text: title
-                    },
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                // the data minimum used for determining the ticks is Math.min(dataMin, suggestedMin)
-                                suggestedMin: 0,
-
-                                // the data maximum used for determining the ticks is Math.max(dataMax, suggestedMax)
-                                suggestedMax: maxY
-                            }
-                        }]
-                    }
-                }
-            };
-            var ctx = document.getElementById(canvasid).getContext("2d");
-            window.myLine = new Chart(ctx, config);
-        };
-
-
-
     }]);
-
-
-
-
-
-
-
-
-    function getMaxOfArray(numArray) {
-      return Math.max.apply(null, numArray);
-    }
-
 })();
